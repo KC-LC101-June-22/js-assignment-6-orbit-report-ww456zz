3b193a24d2d72b2247a13c0dbb6844759f568f2d
ww456zz <ww456zzdd@gmail.com>
2022-08-31 18:38:32 -0500

    final

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -16,34 +16,4 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
- loopArr () {
-	for (let i =0; i < this.satellites.length; i++) {
-		if (!this.arr.includes(this.satellites[i].type)) {
-			this.arr.push(this.satellites[i].type);
-		}
-	}
-	return this.arr;
-  }
-
-  countByType(type: string): number {
-
-	if (this.satellites) {
-	 for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type == type) {
-			this.count++;
-		 }
-	  }
-	}
-
-	if (this.arr[this.arr.length-1] == type) {
-		let ans = this.count;
-		this.count = 0;
-		return ans;
-	} else {
-		return;
-	}
-}
-
-
-
 }
